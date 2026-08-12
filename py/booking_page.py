758a7cf9524db154fdc507dbe5faa8c454466ff1
Ryan1699 <25700@tgs.school>
2026-08-13 09:53:16 +1200

    Done simply version of booking page. Needed to be improved

--- py/booking_page.py
+++ py/booking_page.py
@@ -9,6 +9,7 @@ class BookingPage(tk.Frame):
         self.trip_type = tk.StringVar(value=" ")
         self.route = tk.StringVar(value=" ")
         self.bunk_count = tk.IntVar(value=0)
+        self.receiling_count = tk.IntVar(value=0)
 
         self.create_widgets()
 
@@ -40,11 +41,36 @@ class BookingPage(tk.Frame):
         self.seat_type_label = tk.Label(self, text="Seat Type:", font=("Arial", 12))
         self.seat_type_label.pack(side="top")
 
+        self.bunk_label = tk.Label(self, text="Bunk Seats:", font=("Arial", 12))
+        self.bunk_label.pack(side="top")
         self.bunk_spinbox= tk.Spinbox(self, from_=0, to=20, textvariable=self.bunk_count, font=("Arial", 11))
         self.bunk_spinbox.pack(side="top")
 
-        self.button_book = tk.Button(self, text="Book", font=("Arial", 12))
+        self.receiling_label = tk.Label(self, text="Receiling Seats:", font=("Arial", 12))
+        self.receiling_label.pack(side="top")
+        self.receiling_spinbox= tk.Spinbox(self, from_=0, to=20, textvariable=self.receiling_count, font=("Arial", 11))
+        self.receiling_spinbox.pack(side="top")
+
+        self.button_book = tk.Button(self, text="Book", font=("Arial", 12), command=self.book_onclick)
         self.button_book.pack(side="bottom")
+
+    def book_onclick(self):
+
+
+        if self.trip_type.get().strip() == "":
+            messagebox.showerror("Error", "Please select a trip type")
+            return
+
+        if self.route.get().strip() == "":
+            messagebox.showerror("Error", "Please select a route")
+            return
+
+        if self.bunk_count.get() == 0 and self.receiling_count.get() == 0:
+            messagebox.showerror("Error", "Please select at least one seat")
+            return
+
+        messagebox.showinfo("Success", f"Booking successful!\nTrip Type: {self.trip_type.get()}\nRoute: {self.route.get()}\nBunk Seats: {self.bunk_count.get()}\nReceiling Seats: {self.receiling_count.get()}")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
